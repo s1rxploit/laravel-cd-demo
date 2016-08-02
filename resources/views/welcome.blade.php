84cf43083f4d09e9723fc3b01b8eb32557904868
Tran Duc Thang <thangtd90@gmail.com>
2016-08-02 10:58:28 +0700

    Fix fail build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome to a Production Site</div>
+                <div class="panel-heading">Welcome to a Demo Site</div>
 
                 <div class="panel-body">
                     Your Application's Landing Page.
