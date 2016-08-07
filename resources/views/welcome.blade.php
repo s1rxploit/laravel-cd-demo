3e782e56554b861a72824ec2704312ae40f1efa2
Tran Duc Thang <thangtd90@gmail.com>
2016-08-07 10:47:08 +0700

    Add message about current environment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            <h1 class="text-primary text-center">You are in <strong>{{ app()->environment() }} </strong> environment!</h1>
+            <br>
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome to a Demo Site</div>
-
                 <div class="panel-body">
                     Your Application's Landing Page.
                 </div>
